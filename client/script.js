1915758a8f2376e262df100120a632a743da92e3
Lidia Kovac <70687866+LidiaKovac@users.noreply.github.com>
2022-10-13 18:01:27 +0200

    aggiunge tutto

--- client/script.js
+++ client/script.js
@@ -1,7 +1,36 @@
-window.onload = async() => {
-    let res = await fetch("http://localhost:3000/")
-    let books = await res.json()
-    document.querySelector("body").innerHTML = `<ul> ${ books.map(book => `<li>${book.title}</li>`).join(" ")} </ul>` 
+window.onload = async () => {
+    //generiamo i bottoni per le pagine 
 
+    let res = await fetch("http://localhost:3000/pagina/1")
+    console.log(res);
+    //fetch => Promise(resolve(books), reject(errore))
+    //no await => Promise<pending> 
+    let books = await res.json()
+    for (let i = 0; i < 69; i++) {
+        let paginationContainer = document.querySelector("nav>ul")
+        paginationContainer.innerHTML += `<li class="page-item page-link" onclick="goToPage(${i + 1}, event)">${i + 1}</li>`
+    }
+    renderBooks(books)
+}
+
+const goToPage = async (page) => {
+    let res = await fetch("http://localhost:3000/pagina/" + page)
+    let books = await res.json()
+    renderBooks(books)
+}
+
+const renderBooks = (books) => {
+    let container = document.querySelector(".container>.row")
+    container.innerHTML = ""
+    for (const book of books) {
+        container.innerHTML += `<div class="col col-lg-3 col-md-4 col-sm-6 col-xs-12"> <div class="card" >
+        <img src="${book.img}" class="card-img-top" alt="${book.title}">
+        <div class="card-body">
+          <h5 class="card-title">${book.title}</h5>
+          <p class="card-text">${book.category}</p>
+          <a href="#" class="btn btn-primary">${book.price}</a>
+        </div>
+      </div> </div>`
+    }
 
 }
--- server/server.js
+++ server/server.js
@@ -7,13 +7,15 @@ const horror = require("./horror.json")
 const romance = require("./romance.json")
 const scifi = require("./scifi.json")
 const allBooks = [...fantasy, ...history, ...horror, ...romance, ...scifi]
+// const allBooks = fantasy.concat(history.concat(horror.concat(romance.concat(scifi))))
 
 class Library {
     constructor(books) {
         this.books = books
     }
-    getAll() {
-        return this.books
+    getAll(pageNumber) {
+        pageNumber -= 1
+        return this.books.slice((10 * pageNumber), (10 * pageNumber) + 10)
     }
     findByAsin(asin) {
         for (const book of this.books) {
@@ -25,8 +27,8 @@ class Library {
     }
     findByCategory(cat) {
         let found = []
-        for(const book of this.books) {
-            if(book.category === cat) found.push(book)
+        for (const book of this.books) {
+            if (book.category === cat) found.push(book)
         }
         return found
         //return this.books.filter(book => book.category === cat)
@@ -38,11 +40,19 @@ let library = new Library(allBooks)
 let app = express()
 app.use(cors())
 
-app.get("/", (req, res) => {
+//http://localhost:3000/pagina/13
+
+app.get("/pagina/:pageNumber", (req, res, next) => {
+    //nota su req.params (non serve che la impariate, è una nozione prettamente backend, però serve per capire il codice)
+    //in questa ruota e in quelle successiva vedrete che alcune parole sono precedute da ":"
+    //queste parole si chiamano "parametri dinamici"
+    //express.js ci assicura di poterli ritrovare nel parametro req, sotto req.params.nomeDelParametro
+    //questo significa che dal client io posso inserire "https://localhost:3000/pagina/1"
+    //il valore di req.params.pageNumber sarà 1. 
     try {
-        res.send(library.getAll())
+        res.send(library.getAll(req.params.pageNumber))
     } catch (error) {
-        res.send(error)
+        next(error)
     }
 })
 
@@ -62,4 +72,4 @@ app.get("/:category", (req, res) => {
     }
 })
 
-app.listen(3000)
+app.listen(3000, ()=> console.log("started"))

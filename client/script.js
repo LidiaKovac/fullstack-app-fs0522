578c07f5633e2a5349baf56d7086fc2a6ad199b1
Lidia Kovac <70687866+LidiaKovac@users.noreply.github.com>
2022-10-13 15:06:17 +0200

    Update script.js

--- client/script.js
+++ client/script.js
@@ -1,4 +1,7 @@
 window.onload = async() => {
     let res = await fetch("http://localhost:3000/")
     let books = await res.json()
+    document.querySelector("body").innerHTML = `<ul> ${ books.map(book => `<li>${book.title}</li>`).join(" ")} </ul>` 
+
+
 }
